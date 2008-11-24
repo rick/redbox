89fd7b8eeb2a1a4cc49dc252d5b50b94c428e28c
Rick Bradley <rick@rickbradley.com>
2008-11-23 22:34:50 -0600

    basic spec helper with rr for mocking

---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+$:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
+
+# this is my favorite way to require, ever
+begin
+  require 'spec'
+rescue LoadError
+  require 'rubygems'
+  gem 'rspec'
+  require 'spec'
+end
+
+begin
+  require 'mocha'
+rescue LoadError
+  require 'rubygems'
+  gem 'rr'
+  require 'rr'
+end
+
+Spec::Runner.configure do |config|
+  config.mock_with :rr
+end
+
